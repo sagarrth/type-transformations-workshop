f0bcd7f90cec27b5421735e9562e9110999e82e7
Sagar Rout <sagarrouth0512@gmail.com>
2023-01-11 02:00:30 +0530

    th-undefined-null-constraints

--- src/03.5-type-helpers-pattern/20.6-not-undefined-or-null-constraint.problem.ts
+++ src/03.5-type-helpers-pattern/20.6-not-undefined-or-null-constraint.problem.ts
@@ -1,4 +1,12 @@
-export type Maybe<T> = T | null | undefined;
+export type Maybe<T extends {}> = T;
+
+/**
+ * In this exercise Maybe<null> and Maybe<undefined> was expected to throw error.
+ * Maybe was expected to take any type except null and undefined.
+ * 
+ * I was thinking Maybe<T extends any> = T would have worked, but any also allows null and undefined
+ * {} is used to constrain types except null and undefined
+ */
 
 type tests = [
   // @ts-expect-error
