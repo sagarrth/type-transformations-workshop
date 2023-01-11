0d0ea4616207b56c877b6104121a260dab2332b4
Sagar Rout <sagarrouth0512@gmail.com>
2023-01-11 10:27:37 +0530

    th-nonempty-array

--- src/03.5-type-helpers-pattern/20.7-non-empty-array.problem.ts
+++ src/03.5-type-helpers-pattern/20.7-non-empty-array.problem.ts
@@ -1,4 +1,14 @@
-type NonEmptyArray = unknown;
+type NonEmptyArray<T> = [T, ...T[]];
+// (or)
+// type NonEmptyArray<T> = [T, ...Array<T>]
+
+/**
+ * Array<T> would have made the type to accept an empty array too
+ * In this exercise the requirement was to enforce a non empty array, minimum 1 element should be present
+ * That is done by [T, ...Array<T>], the spreading of Array<T> makes it take any arbitary number of elements
+ * Had the requirement been to have minimum 2 or 3 elements in the array then the types would have been - 
+ * [T, T, ...Array<T>] and [T, T, T, ...Array<T>] accordingly
+ */
 
 export const makeEnum = (values: NonEmptyArray<string>) => {};
 
