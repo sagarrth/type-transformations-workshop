54f73013574e20ba753b4260ba5580697cbd8847
Sagar Rout <sagarrouth0512@gmail.com>
2023-01-11 10:30:11 +0530

    Type helpers pattern (#4)
    * th-constraints
    
    * th-undefined-null-constraints
    
    * th-nonempty-array

--- src/03.5-type-helpers-pattern/20.2-constraints.problem.ts
+++ src/03.5-type-helpers-pattern/20.2-constraints.problem.ts
@@ -1,6 +1,13 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type AddRoutePrefix<TRoute> = `/${TRoute}`;
+/**
+ * learning from this exercise is the use of @ts-expect-error directive
+ * the directive suppresses the error where expected and complains about its usage if unnecessary
+ * 
+ * In this exercise AddRoutePrefix<boolean> was expected to throw error
+ * That can be done by constraining the generic type to string, any other type should throw error
+ * */ 
+type AddRoutePrefix<TRoute extends string> = `/${TRoute}`;
 
 type tests = [
   Expect<Equal<AddRoutePrefix<"">, "/">>,
