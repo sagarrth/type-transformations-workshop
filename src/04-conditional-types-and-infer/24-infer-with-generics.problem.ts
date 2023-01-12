c7d7bb8cf2da9d745bc0b8b2c31a7296ef9c570a
Sagar Rout <sagarrouth0512@gmail.com>
2023-01-12 23:22:06 +0530

    cti-infer-generics-templateliteral-extraction

--- src/04-conditional-types-and-infer/24-infer-with-generics.problem.ts
+++ src/04-conditional-types-and-infer/24-infer-with-generics.problem.ts
@@ -14,6 +14,11 @@ type Example = MyComplexInterface<
   { x: 12; y: 14 }
 >;
 
-type GetPoint<T> = unknown;
+type GetPoint<T> = T extends MyComplexInterface<infer A, infer B, infer C, infer D> ? D: never;
+/**
+ * the type which needs to be returned can be inferred, other types can be kept as any
+ * so the good solution is - 
+ * type GetPoint<T> = T extends MyComplexInterface<any, any, any, infer D> ? D: never;
+ */
 
 type tests = [Expect<Equal<GetPoint<Example>, { x: 12; y: 14 }>>];
